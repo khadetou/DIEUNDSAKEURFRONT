b57853bd4f1b7b86e71b005cbf03f31d49f8d90e
khadetou <khadetou96@gmail.com>
2022-11-01 15:17:05 +0000

    notfound page

---
+++ src/components/dashboard/auth/404/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const NotfoundBody = () => {
+  return (
+    <div className="page-wrapper">
+      <div className="page-not-found">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-6 col-md-8 col-sm-10">
+              <div className="not-found-img">
+                <img
+                  src="https://themes.pixelstrap.com/sheltos/assets/images/svg/3.svg"
+                  className="img-fluid"
+                  alt=""
+                />
+                <div className="animation-error">
+                  <img
+                    src="https://themes.pixelstrap.com/sheltos/assets/images/svg/error.svg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-8 col-sm-10">
+              <div className="not-found-content">
+                <h2>Sorry...This page is gone.</h2>
+                <p className="font-roboto light-font">
+                  We are sorry but the page you are looking for doesn't exist or
+                  has been removed. please check or search again.
+                </p>
+                <div className="btns">
+                  <a
+                    href="index.html"
+                    className="btn btn-pill btn-gradient color-4"
+                  >
+                    go to home page
+                  </a>
+                  <a
+                    href="index.html"
+                    className="btn btn-pill btn-dashed color-4 ms-2"
+                  >
+                    Report problem
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotfoundBody;
---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import NotfoundBody from "components/dashboard/auth/404";
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <>
+      <NotfoundBody />
+    </>
+  );
+};
+
+export default Notfound;
